3316c62fe552ee48800e8a532070f2298c990a2f
asiaaitch <asiawill06@gmail.com>
2023-02-11 20:49:58 -0500

    Coding Challenge 2-11

---
+++ 8kyu/String repeat.js
@@ -0,0 +1,9 @@
+// // String repeat
+
+// DESCRIPTION:
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// My Solution
+function repeatStr (n, s) {
+    return(s.repeat(n));
+  }
