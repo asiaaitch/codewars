9ef33c34cbdaefea8a41ca5c19d10bee31c10860
asiaaitch <asiawill06@gmail.com>
2023-02-15 21:19:36 -0500

    Coding Challenge 2-15

---
+++ Remove String Spaces.js
@@ -0,0 +1,11 @@
+// // Remove String Spaces
+
+// DESCRIPTION:
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// My Solution
+
+function noSpace(x){
+    return x.replace(/\s/g, '');
+    }
+    
