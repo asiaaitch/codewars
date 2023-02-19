3145b5b6dc67ac899d2f30b0b8c06c53283c01bb
asiaaitch <asiawill06@gmail.com>
2023-02-18 19:46:45 -0500

    Coding Challenge 2-18

---
+++ 8kyu/Reversed Strings.js
@@ -0,0 +1,10 @@
+// // Reversed Strings
+
+// DESCRIPTION:
+// Complete the solution so that it reverses the string passed into it.
+
+// My Solution
+
+function solution(str){
+    return str.split('').reverse().join('');
+  }
