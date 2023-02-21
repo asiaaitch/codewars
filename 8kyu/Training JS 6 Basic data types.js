1e45b1e9d7ac1011cbdf88aea7b4c282941b561b
asiaaitch <asiawill06@gmail.com>
2023-02-20 20:54:04 -0500

    Coding Challenge 2-20

---
+++ 8kyu/Training JS 6 Basic data types.js
@@ -0,0 +1,18 @@
+// // Training JS #6: Basic data types--Boolean and conditional statements if..else
+
+// DESCRIPTION:
+// In javascript, Boolean object only has two value, true or false. Use ===, ==, >, <, >=, <= between two variables(or constant), the result of such an expression is a bool value. The operation between the two bool values can be used with logical operators:&&, ||, ! or some bitwise operators:&, |, ^
+
+// Many of the data are likely to be implicitly converted to Boolean values in a given context, most common in conditional statements. In other words, the Boolean value is mainly used for conditional judgment. 
+
+// My Solution
+
+function trueOrFalse(val){
+    if(val === false || val === 0 || val === -0 || val === '' || val === null ||
+       val === undefined || val === NaN){
+      return "false";
+    }else {
+      return "true";
+    }
+  }
+  
