b72eac3285895da8f8cfdbd6917653d7a82db896
asiaaitch <asiawill06@gmail.com>
2023-02-22 21:27:30 -0500

    You Cant Code Under Pressure1

---
+++ 8kyu/You Cant Code Under Pressure1.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Code as fast as you can! You need to double the integer and return it.
+
+// My Solution
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    i = i * 2
+    return i;
+  }
