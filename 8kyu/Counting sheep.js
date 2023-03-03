28e5acf375e6f45fdb1ee25e619d13dd25437513
asiaaitch <asiawill06@gmail.com>
2023-03-02 19:20:50 -0500

    Coding Challenge 3-2

---
+++ 8kyu/Counting sheep.js
@@ -0,0 +1,10 @@
+// // Counting sheep...
+// DESCRIPTION:
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// // My Solution
+
+function countSheeps(arrayOfSheep) {
+    // TODO May the force be with you
+    return arrayOfSheep.filter(Boolean).length;
+  }
