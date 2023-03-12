2c7f01093bac358af44c6a6c6f5b8332f076d1d0
asiaaitch <asiawill06@gmail.com>
2023-03-12 18:12:23 -0400

    Training JS 5 Basic data types--Object

--- 8kyu/Training JS 5 Basic data types--Object.js
+++ 8kyu/Training JS 5 Basic data types--Object.js
@@ -7,6 +7,10 @@
 // // "This white dog has 4 legs."
 // // When you have finished the work, click "Run Tests" to see if your code is working properly.
 
-// // In the end, click "Submit" to submit your code pass this kata.// My Solution
+// // In the end, click "Submit" to submit your code pass this kata.
 
 // My Solution
+
+function animal(obj){
+    return 'This ' + obj.color + " "+ obj.name + " "+ 'has ' + obj.legs + ' legs.'
+  }
