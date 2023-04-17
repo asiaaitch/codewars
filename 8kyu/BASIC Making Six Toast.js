7ef0b706ab3020fe3cff829aa67b40c28fbfae8f
asiaaitch <asiawill06@gmail.com>
2023-04-16 22:49:05 -0400

    BASIC Making Six Toast

---
+++ 8kyu/BASIC Making Six Toast.js
@@ -0,0 +1,8 @@
+// You are going to make toast fast, you think that you should make multiple pieces of toasts and once. So, you try to make 6 pieces of toast.
+
+// My Solution
+
+
+function sixToast(num) {
+    return Math.abs(num-6)
+  }
