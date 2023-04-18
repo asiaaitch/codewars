14076eedc185b62922e9af50172b566b89d77b73
asiaaitch <asiawill06@gmail.com>
2023-04-17 22:49:56 -0400

    Crash Override

---
+++ 8kyu/Crash Override.js
@@ -0,0 +1,16 @@
+
+// Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+
+// My Solution
+
+
+const initialCap = (str) => str[0].toUpperCase();
+const isValidName = (name) => /^[a-z]/i.test(name);
+const aliasGen = (fName, lName) => {
+  return (isValidName(fName) && isValidName(lName))
+    ? `${ firstName[initialCap(fName)] } ${ surname[initialCap(lName)] }`
+    : 'Your name must start with a letter from A - Z.';
+}
