fcb74f349a6564af06e55916945fa1281bcdbbe0
asiaaitch <asiawill06@gmail.com>
2023-05-04 20:57:55 -0400

    Negative Interger.js

---
+++ 8kyu/Negative Integer.js
@@ -0,0 +1,11 @@
+// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// My Solution
+
+function makeNegative(num) {
+    if (num === Math.abs(num)){
+      return -Math.abs(num)
+    }else
+      return -Math.abs(num)
+  
+  }
