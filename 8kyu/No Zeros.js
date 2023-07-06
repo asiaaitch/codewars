9ed579ed0d42749d3dc3d57d1d62f66dadb30197
asiaaitch <asiawill06@gmail.com>
2023-07-06 17:52:50 -0400

    Add Length

---
+++ 8kyu/No Zeros.js
@@ -0,0 +1,14 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// My Solution
+
+function noBoringZeros(n) {
+    while (n % 10 === 0 && n !== 0) {
+      n /= 10;
+    }
+    return n;
+  }
